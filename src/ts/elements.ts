fa5a53f548599789f5e6707669082b39a44b00ee
Lucas Söderlund <superlucke123@gmail.com>
2025-01-10 11:50:25 +0100

    update1

---
+++ src/ts/elements.ts
@@ -0,0 +1,19 @@
+import { Product } from "./interface.js";
+
+const products: Product[] = [
+   { title: 'Greta Fury', 
+    category: 'Skateboard', 
+    sex: 'Unisex', 
+    size: ['S', 'M', 'L'], 
+    description: 'lorem ipsumn lkada ölaksdölaks ölajkdsöla akjsdfdhaa',
+    price: 799,
+}
+
+   { title: 'Greta Fury', 
+    category: 'Skateboard', 
+    sex: 'Unisex', 
+    size: ['S', 'M', 'L'], 
+    description: 'lorem ipsumn lkada ölaksdölaks ölajkdsöla akjsdfdhaa',
+    price: 799,
+}
+]
---
+++ src/ts/interface.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    title: string,
+    category: string,
+    sex: string,
+    size?: string[],
+    description: string,
+    price: number,
+    image?: string 
+}
